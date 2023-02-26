8708bd36cfc39af00a3b7f7924e7544f98e695ae
SauravDhiman98 <96328417+SauravDhiman98@users.noreply.github.com>
2023-02-26 11:59:18 +0530

    Making app for Smp license verification

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Cors = require("cors");
+const dotenv = require("dotenv");
+require("events").EventEmitter.defaultMaxListeners = 15;
+
+dotenv.config();
+mongoose.set("strictQuery", true);
+app.use(express.json());
+app.use(
+  Cors({
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+mongoose.connect(process.env.Mongo_DB_URL)
+// {
+//     dbName: "Helium_DB",
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+  .then(() => console.log("Congrulations your are connected to database..."))
+  .catch((err) => console.log(err));
+
+//app.use("/smplcnsvrftn/");
+
+app.listen(process.env.PORT || 4500, () => {
+  console.log("You are live now....");
+});
