e92d58ff938061166e0b08a32934d4710da212dc
Shubham Kumar Saurav Dhiman <XDNR84@motorolasolutions.com>
2023-04-17 02:51:14 +0530

    Added UI and Bckend Code

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const Cors = require("cors");
 const dotenv = require("dotenv");
+const GetAllocation = require("./controllers/GetAllocationForSIT");
+const GetAllocationForUAT = require("./controllers/GetAllocationForUAT")
 require("events").EventEmitter.defaultMaxListeners = 15;
 
 dotenv.config();
@@ -16,16 +18,16 @@ app.use(
   })
 );
 
-mongoose.connect(process.env.Mongo_DB_URL)
-// {
-//     dbName: "Helium_DB",
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-  .then(() => console.log("Congrulations your are connected to database..."))
-  .catch((err) => console.log(err));
+// mongoose.connect(process.env.Mongo_DB_URL)
+// // {
+// //     dbName: "Helium_DB",
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true,
+// // })
+//   .then(() => console.log("Congrulations your are connected to database..."))
+//   .catch((err) => console.log(err));
 
-//app.use("/smplcnsvrftn/");
+app.use("/smplcnsvrftn", GetAllocation, GetAllocationForUAT);
 
 app.listen(process.env.PORT || 4500, () => {
   console.log("You are live now....");
